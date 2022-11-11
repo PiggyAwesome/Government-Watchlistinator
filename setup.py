c6747e071c10f5eabe82546de47de43f132db38d
PiggyAwesome <48888771+PiggyAwesome@users.noreply.github.com>
2022-11-11 15:22:45 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='watchlistinator',
+    version='0.1',
+    license='MIT',
+    author="PiggyAwesome",
+    author_email='businesspiggyawesome@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/PiggyAwesome/Government-Watchlistinator/',
+    keywords='sketchy', 'search', "goverment, 'watchlist'
+    install_requires=[
+      ],
+
+)
